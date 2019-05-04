c5b84ea075d72fd9ace976abd5d85e177e2968f2
Pooja Manuja <poojamanuja@Poojas-MBP.attlocal.net>
2019-05-04 19:28:44 -0400

    added date transformation logic in new page

--- controllers/appointment_controller.js
+++ controllers/appointment_controller.js
@@ -2,6 +2,55 @@ const express = require('express');
 const appointment = express.Router();
 const Appointment = require('../models/appointments.js');
 
+//function that returns month name
+const getMonthName = (monthNum) => {
+  let month = '';
+  switch (monthNum){
+    case 0:
+      month = 'January';
+      break;
+    case 1:
+        month = 'February';
+        break;
+    case 2:
+      month = 'March';
+      break;
+    case 3:
+      month = 'April';
+      break;
+    case 4:
+        month = 'May';
+        break;
+    case 5:
+      month = 'June';
+      break;
+    case 6:
+      month = 'July';
+      break;
+    case 7:
+        month = 'August';
+        break;
+    case 8:
+      month = 'September';
+      break;
+    case 9:
+      month = 'October';
+      break;
+    case 10:
+        month = 'November';
+        break;
+    case 11:
+      month = 'December';
+      break;
+
+    default:
+    month: 'invalid';
+    break;
+  }
+
+  return month;
+}
+
 //function returns day of week based on input day number
 const getDay = (dayNum) => {
   let day ='';
@@ -44,6 +93,31 @@ const getDay = (dayNum) => {
   return day;
 }
 
+//get date as format 'May 30, 2019'
+const getFormattedDate = (date) => {
+
+  let dateFormatted='';
+
+  let day = date.getDay();
+  day = getDay(parseInt(day));
+  console.log(day);
+
+  let mm = date.getMonth();
+  console.log('mm is ' , mm);
+  mm = getMonthName(parseInt(mm));
+  console.log('mm is ' , mm);
+
+  let dd = String(date.getDate() + 1);
+  let yyyy = date.getFullYear();
+
+
+  dateFormatted = day + ', ' + mm + ' ' + dd + ',' + yyyy;
+  console.log('Formatted date is ', dateFormatted);
+
+  return dateFormatted;
+}
+
+
 //delete appointment from db
 appointment.delete('/:id',(req, res) => {
 
@@ -71,12 +145,17 @@ appointment.get('/new',(req, res) => {
 
 //post a new appointment in db
 appointment.post('/new',(req, res) => {
+  console.log('re body before' ,req.body);
 
   let date = new Date(req.body.date);
-  let day = date.getDay();
-  console.log('day today is : ' , day);
-  console.log(getDay(parseInt(day)));
-  // console.log('day today is : ' , day);
+  let dateFormatted = getFormattedDate(date);
+
+  console.log('dateFormatted :' , dateFormatted);
+  console.log('original date :' , req.body.date);
+  req.body.date = dateFormatted;
+
+  console.log('req body after' , req.body);
+
   Appointment.create(req.body , (err, newAppointment) => {
       console.log(newAppointment);
       res.redirect('/');
